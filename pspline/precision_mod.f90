13eff45aeee3208b034583a44acb60f914fdc836
Jai Sachdev <jsachdev@pppl.gov>
2019-12-23 13:48:15 -0500

    Repo has been moved.

--- pspline/precision_mod.f90
+++
@@ -1,9 +0,0 @@
-module psp_precision_mod
-  use iso_c_binding, only: c_int, c_float, c_double
-  integer, parameter :: ip = c_int
-#ifdef _SINGLE_PRECISION
-  integer, parameter :: fp = c_float
-#else
-  integer, parameter :: fp = c_double
-#endif
-end module psp_precision_mod
